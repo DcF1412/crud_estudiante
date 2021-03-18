06375b5935a5985e0e5c445a87dae2d1bd6f54b1
Reynier <reynier.ramos.p@gmail.com>
2021-03-18 19:59:37 +0100

    varios metodos de filtrar listas

--- src/main/java/com/dairon/crud_estudiante/repository/EstudianteRepository.java
+++ src/main/java/com/dairon/crud_estudiante/repository/EstudianteRepository.java
@@ -1,11 +1,24 @@
 package com.dairon.crud_estudiante.repository;
 
+import java.util.List;
+
 import com.dairon.crud_estudiante.domain.Estudiante;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante, Long>{
-    
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+
+    // operador -> = igual, <> distinto, > mayor, < menor, <=, >=
+    // select [<campo1, campo2, campoN> or * <para todas las columnas>] from [tabla]
+    // [where campoX {operador} valor]
+
+    // query en dql
+    @Query("select e from Estudiante e where e.especialidad = :especialidad") // select * from estudiante where especialidad='Chofer'
+    List<Estudiante> loquenosdalagana(String especialidad);
+
+    // lazy loading
+    List<Estudiante> findAllByEdad(int edad);
 }
